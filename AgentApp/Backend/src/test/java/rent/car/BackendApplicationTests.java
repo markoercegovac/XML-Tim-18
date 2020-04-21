6d2daa69a3f3d857c69c182c5af5c39cf008f8ea
Erceg <prolomercegovac@yahoo.com>
2020-04-21 14:22:11 +0200

    Initial angular app

---
+++ AgentApp/Backend/src/test/java/rent/car/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rent.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
