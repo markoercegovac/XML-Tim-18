63cf5bc95c7feb5ce1af6416599336162adeac47
tiberius <tiberlas1997@gmail.com>
2020-04-24 15:29:03 +0200

    added sql script for start up

---
+++ AgentApp/Backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+--SCRIPT FOR ADDING DEFAULT DATA TO AGENT APP
+
+insert into car_brand(id, name, is_removed) values(1, "BMW", FALSE);
+insert into car_brand(id, name, is_removed) values(2, "AUDI", FALSE);
+insert into car_brand(id, name, is_removed) values(3, "PORSCHE", FALSE);
+insert into car_brand(id, name, is_removed) values(4, "YUGO", FALSE);
+insert into car_brand(id, name, is_removed) values(5, "LAMBORGHINI", FALSE);
+insert into car_brand(id, name, is_removed) values(6, "TOYOTA", FALSE);
+insert into car_brand(id, name, is_removed) values(7, "FERRARI", FALSE);
+insert into car_brand(id, name, is_removed) values(8, "FIAT", FALSE);
