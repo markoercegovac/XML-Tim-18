a1597e4da3e3d36263730c9270cd9a3c94670300
tiberius <tiberlas1997@gmail.com>
2020-05-24 12:14:23 +0200

    Eureka configured

--- MicroserviceApp/Backend/AdvertSearch/src/main/java/com/example/advert_search/AdvertSearchApplication.java
+++ MicroserviceApp/Backend/AdvertSearch/src/main/java/com/example/advert_search/AdvertSearchApplication.java
@@ -2,8 +2,10 @@ package com.example.advert_search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdvertSearchApplication {
 
     public static void main(String[] args) {
