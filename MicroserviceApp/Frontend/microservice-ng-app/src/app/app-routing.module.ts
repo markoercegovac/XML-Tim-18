9600ad070ac4cc99b576b7897bb984826f28882e
Erceg <prolomercegovac@yahoo.com>
2020-05-29 16:53:53 +0200

    Resolved conflict ng stable

--- MicroserviceApp/Frontend/microservice-ng-app/src/app/app-routing.module.ts
+++ MicroserviceApp/Frontend/microservice-ng-app/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RegistrationComponent} from './registration/registration.component';
 import {LoginComponent} from './login/login.component';
+import {CreateAdvertComponent} from "./create-advert/create-advert.component";
+import {MyAdvertsComponent} from "./my-adverts/my-adverts.component";
+import {AllAdvertsComponent} from "./all-adverts/all-adverts.component";
 
 
 const routes: Routes = [
@@ -12,7 +15,20 @@ const routes: Routes = [
   {
     path: 'user-manager/login',
     component: LoginComponent
+  },
+  {
+    path: 'create/advert',
+    component: CreateAdvertComponent
+  },
+  {
+    path: 'my/adverts',
+    component: MyAdvertsComponent
+  },
+  {
+    path: 'all/adverts',
+    component: AllAdvertsComponent
   }
+
 ];
 
 @NgModule({
--- MicroserviceApp/Frontend/microservice-ng-app/src/app/app.module.ts
+++ MicroserviceApp/Frontend/microservice-ng-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
@@ -10,19 +10,26 @@ import { RegistrationComponent } from './registration/registration.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LoginComponent } from './login/login.component';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
+import { CreateAdvertComponent } from './create-advert/create-advert.component';
+import { MyAdvertsComponent } from './my-adverts/my-adverts.component';
+import { AllAdvertsComponent } from './all-adverts/all-adverts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
     NavigationComponent,
-    LoginComponent
+    LoginComponent,
+    CreateAdvertComponent,
+    MyAdvertsComponent,
+    AllAdvertsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
