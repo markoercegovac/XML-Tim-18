b9c9e11d971b26f8175716b06c2d6488433b5970
Erceg <prolomercegovac@yahoo.com>
2020-06-02 14:47:05 +0200

    Wip

---
+++ MicroserviceApp/Backend/AdvertManager/src/main/java/com/example/advertmanagerapp/dto/CreateAdvertDto.java
@@ -0,0 +1,4 @@
+package com.example.advertmanagerapp.dto;
+
+public class CreateAdvertDto {
+}
---
+++ MicroserviceApp/Backend/AdvertManager/src/main/java/com/example/advertmanagerapp/service/impl/AdvertServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.advertmanagerapp.service.impl;
+
+public class AdvertServiceImpl {
+}
