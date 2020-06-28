43087e486fb651e66fdc6d009911896376b3cc8e
ercegovacmarko <ercegovacmarko@yahoo.com>
2020-06-28 22:45:58 +0200

    wip

--- AgentApp/Backend/src/main/resources/data.sql
+++ AgentApp/Backend/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 --SCRIPT FOR ADDING DEFAULT DATA TO AGENT APP
-
---CLIENT
-insert into client(email, name, surname)
-values('mika@maildrop.cc', 'mika', 'mikic'),
-   ('pera@maildrop.cc', 'pera', 'peric');
+--
+-- --CLIENT
+-- insert into client(email, name, surname)
+-- values('mika@maildrop.cc', 'mika', 'mikic'),
+--    ('pera@maildrop.cc', 'pera', 'peric');
 
 --CAR BRAND
 insert into car_brand(id, name, is_removed) values(1, "BMW", FALSE);
@@ -45,18 +45,18 @@ insert into car_fuel_type(id, fuel_type,is_removed) values(5,"FULL-ELECTRIC",FAL
 insert into car_fuel_type(id, fuel_type,is_removed) values(6,"HYDROGEN",FALSE);
 insert into car_fuel_type(id, fuel_type,is_removed) values(7,"OTHER",FALSE);
 
--- CAR
-insert into car(id, children_sit_number, is_insurance, travel_distance_constraint, car_brand_id, car_class_id, car_fuel_type_id, car_model_id, car_transmission_type_id, mileage_id) values
-(301, 2, false, 4333, 1, 1, 1, 1, 1, null),
-(302, 2, false, 4333, 2, 2, 2, 2, 2, null),
-(303, 2, false, 4333, 1, 1, 1, 3, 2, null);
-
--- PRICE
-insert into price(id, name, price_per_day, discount, distance_overflow_price, insurance_price, is_removed) values
-(801, "price #1", 432.4, 30, 452445, 4334, false),
-(802, "price #2", 432.4, 30, 452445, 4334, false),
-(803, "price #3", 432.4, 30, 452445, 4334, false),
-(804, "price #4", 432.4, 30, 452445, 4334, false);  
+-- -- CAR
+-- insert into car(id, children_sit_number, is_insurance, travel_distance_constraint, car_brand_id, car_class_id, car_fuel_type_id, car_model_id, car_transmission_type_id, mileage_id) values
+-- (301, 2, false, 4333, 1, 1, 1, 1, 1, null),
+-- (302, 2, false, 4333, 2, 2, 2, 2, 2, null),
+-- (303, 2, false, 4333, 1, 1, 1, 3, 2, null);
+--
+-- -- PRICE
+-- insert into price(id, name, price_per_day, discount, distance_overflow_price, insurance_price, is_removed) values
+-- (801, "price #1", 432.4, 30, 452445, 4334, false),
+-- (802, "price #2", 432.4, 30, 452445, 4334, false),
+-- (803, "price #3", 432.4, 30, 452445, 4334, false),
+-- (804, "price #4", 432.4, 30, 452445, 4334, false);
 
 
 
