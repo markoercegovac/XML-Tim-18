d1038dac9c3fabc1b9fc63efc7e4ca1b1b01cb89
tiberius <tiberlas1997@gmail.com>
2020-07-02 15:46:05 +0200

    Refactor mail service on MS

--- MicroserviceApp/Backend/MailManager/src/main/java/com/team18/xml/MailManager/service/MyMailSender.java
+++ MicroserviceApp/Backend/MailManager/src/main/java/com/team18/xml/MailManager/service/MyMailSender.java
@@ -1,26 +1,61 @@
 package com.team18.xml.MailManager.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyMailSender {
 
-	@Autowired
-	private JavaMailSender javaMailSender;
+	@Value("${spring.mail.username}")
+	private String username;
+	@Value("${spring.mail.password}")
+	private String password;
+	@Value("${spring.mail.host}")
+	private String host;
+	@Value("${spring.mail.port}")
+	private String port;
 
 	public void send(String receiver, String header, String content) {
 
-		content += "\n\nThis message is auto generated.\nBest regards,\nRENT IT application";
+        Properties properties=new Properties();
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", port);
+		
+		Session session=Session.getInstance(properties,new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+		});
+		
+        try {
+            Message message=new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(receiver));
 
-		SimpleMailMessage msg = new SimpleMailMessage();
-		msg.setTo(receiver);
-		msg.setSubject(header);
-		msg.setText(content);
-		msg.setFrom("rent-it@maildrop.cc");
 
-		javaMailSender.send(msg);
+			content += "\n\nThis message is auto generated.\nBest regards,\nRENT IT application";
+            message.setSubject(header);
+            message.setContent(content,"text/html; charset=utf-8");
+            message.setText(content);
+
+
+            Transport.send(message);
+
+        }catch(MessagingException e) {
+            throw new RuntimeException(e);
+        }
 	}
 }
