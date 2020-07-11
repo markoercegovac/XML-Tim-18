878947ca3b8e1a2ee1068bf8b1698b3917e84dd3
tiberius <tiberlas1997@gmail.com>
2020-07-11 21:55:39 +0200

    Agent is over

--- AgentApp/Frontend/rent-ng-app/src/app/app.component.ts
+++ AgentApp/Frontend/rent-ng-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './service/AuthService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rent-ng-app';
+
+  constructor(
+    private auth: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.auth.onLoadNavigation();
+  }
+
 }
